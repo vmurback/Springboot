a36ebcd16b38618c7eb7cb9fd629946e676a78f9
vmurback <83590663+vmurback@users.noreply.github.com>
2021-05-26 21:44:30 -0300

    Add files via upload

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -0,0 +1,29 @@
+package org.generation.blogPessoal.controller;
+
+import java.util.List;
+
+import org.generation.blogPessoal.model.Postagem;
+import org.generation.blogPessoal.repository.postagemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/postagem")
+@CrossOrigin("*")
+public class PostagemController {
+	
+	@Autowired
+	private postagemRepository repositoty;
+	
+	@GetMapping
+	public ResponseEntity<List<Postagem>> GetAll (){
+		
+		
+		return ResponseEntity.ok(repositoty.findAll());
+	}
+
+}
