76163f2d9e117d95cf01d7f54df371594cc25f7f
vmurback <83590663+vmurback@users.noreply.github.com>
2021-05-29 00:00:52 -0300

    Exercicios 28-05

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -21,48 +21,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/postagem")
 @CrossOrigin("*")
 public class PostagemController {
-	
+
 	@Autowired
 	private postagemRepository repositoty;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Postagem>> GetAll (){
-		
-		
+	public ResponseEntity<List<Postagem>> GetAll() {
+
 		return ResponseEntity.ok(repositoty.findAll());
 	}
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<Postagem> GetById(@PathVariable long id){
-		return repositoty.findById(id).map(resp ->ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
-		
+	public ResponseEntity<Postagem> GetById(@PathVariable long id) {
+		return repositoty.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+
 	}
-	
+
 	@GetMapping("/titulo/{titulo}")
-	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo){
+	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo) {
 		return ResponseEntity.ok(repositoty.findAllByTituloContainingIgnoreCase(titulo));
-		
-				
+
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem){
+	public ResponseEntity<Postagem> post(@RequestBody Postagem postagem) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(repositoty.save(postagem));
-		
-				
+
 	}
-	
+
 	@PutMapping
-	public ResponseEntity<Postagem> put (@RequestBody Postagem postagem){
+	public ResponseEntity<Postagem> put(@RequestBody Postagem postagem) {
 		return ResponseEntity.status(HttpStatus.OK).body(repositoty.save(postagem));
-	
+
 	}
-	
+
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable long id) {
 		repositoty.deleteById(id);
-		
-		
+
 	}
 }
-
